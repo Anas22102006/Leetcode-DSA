53437e4cbb34dfcfa7946d64579c16db5a3cc461
Anas Usman Ali <145250917+Anas22102006@users.noreply.github.com>
2026-03-04 22:13:19 +0530

    Time: 1272 ms (54.72%) | Memory: 471.3 MB (90.57%) - LeetSync

---
+++ 336-palindrome-pairs/palindrome-pairs.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPal(const string& s, int l, int r){
+        while(l < r) if(s[l++] != s[r--]) return false;
+        return true;
+    }
+
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        unordered_map<string,int> pos;
+        int n = words.size();
+        for(int i=0;i<n;i++) pos[words[i]] = i;
+
+        vector<vector<int>> ans;
+
+        for(int i=0;i<n;i++){
+            string &w = words[i];
+            int L = w.size();
+
+            for(int k=0;k<=L;k++){
+                // split: left = w[0..k-1], right = w[k..L-1]
+                // case 1: left is palindrome, need rev(right) in front
+                if(isPal(w, 0, k-1)){
+                    string right = w.substr(k);
+                    reverse(right.begin(), right.end());
+                    auto it = pos.find(right);
+                    if(it != pos.end() && it->second != i){
+                        ans.push_back({it->second, i});
+                    }
+                }
+
+                // case 2: right is palindrome, need rev(left) at end
+                if(k != L && isPal(w, k, L-1)){
+                    string left = w.substr(0, k);
+                    reverse(left.begin(), left.end());
+                    auto it = pos.find(left);
+                    if(it != pos.end() && it->second != i){
+                        ans.push_back({i, it->second});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
